4ec1e83725ce98565fb1002a231d8af231a6f178
Diego Robles <2993323220101@ingenieria.usac.edu.gt>
2022-02-10 12:19:58 -0600

    Creacion del proyecto

---
+++ src/Proyecto1_Compi/Proyecto1.java
@@ -0,0 +1,12 @@
+package Proyecto1_Compi;
+
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        Menu nuevo = new Menu();
+        nuevo.setVisible(true);
+        
+    }
+
+}
+
